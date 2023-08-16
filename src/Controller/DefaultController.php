b50d1932fefed2a89ed6b2c9d0713a8dc8e45843
MarieClaireE <mcemma.974@gmail.com>
2023-08-16 12:36:44 +0200

    codacy

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,46 +1,47 @@
 <?php
 
-	namespace App\Controller;
+namespace App\Controller;
 
-	use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-	use Symfony\Component\HttpFoundation\Response;
-	use Symfony\Component\Routing\Annotation\Route;
-	use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 
-	class DefaultController extends AbstractController
-	{
+class DefaultController extends AbstractController
+{
 
-				/**
-				 * @var Environment $twig
-				 */
-				private $twig;
+			/**
+			 * @var Environment $twig
+			 */
+			private $twig;
 
-				/**
-				 * construct DefaultController
-				 * @var Environment $twig
-				 */
-				public function __construct(Environment $twig)
-				{
+			/**
+			 * construct DefaultController
+			 * @var Environment $twig
+			 */
+			public function __construct($twig)
+			{
 
-					$this->twig = $twig;
-					return $this;
+				$this->twig = $twig;
+				return $this;
 
-					//end __construct
-				}
+				//End __construct()
 
-				/**
-				 * @return Response
-				 */
-				#[Route("/", name:"homepage")]
-				public function indexAction(): Response
-				{
+			}
 
-					return new Response($this->twig->render('default/index.html.twig',
-						['user' => $this->getUser()]
-					));
+			/**
+			 * @return Response
+			 */
+			#[Route("/", name:"homepage")]
+			public function indexAction(): Response
+			{
 
-				}
+				return new Response($this->twig->render('default/index.html.twig',[
+					'user' => $this->getUser()
+					])
+				);
 
-	}
-	
+			}
+		
+}
